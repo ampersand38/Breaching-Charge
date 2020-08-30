c53cb4a7273a911e615864f372f251225550d96b
ampersand38 <ampersand38@gmail.com>
2020-08-30 00:18:43 -0400

    ACE structure fixes

--- addons/main/XEH_PREP.hpp
+++ addons/main/XEH_PREP.hpp
@@ -1,5 +1,5 @@
 // testing, update fncs on the fly
-#define PREP(var1) TRIPLES(ADDON,fnc,var1) = { call compile preProcessFileLineNumbers '\MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\functions\DOUBLES(fnc,var1).sqf' }
+//#define PREP(var1) TRIPLES(ADDON,fnc,var1) = { call compile preProcessFileLineNumbers '\MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\functions\DOUBLES(fnc,var1).sqf' }
 
 PREP(breachObstacle);
 PREP(getDoor);
